2b12104150a22c3d3da3eb75194d92cc389a22ae
ketan062 <ketankhattar062@gmail.com>
2024-10-23 16:57:59 +0530

    step2 - add skeleton route

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+
+app.post("/user/signup",function(req,res){
+    res.json(({
+        mess:"signup endpoint"
+    }))
+})
+app.post("/user/signin",function(req,res){
+    res.json(({
+        mess:"signup endpoint"
+    }))
+})
+app.get("/user/purchases",function(req,res){
+    res.json(({
+        mess:"signup endpoint"
+    }))
+})
+app.post("/course/purchase",function(req,res){
+    // you would expect user to pay you money
+    res.json(({
+        mess:"signup endpoint"
+    }))
+})
+app.get("/courses",function(req,res){
+    res.json(({
+        mess:"signup endpoint"
+    }))
+})
+
+
+
+
+app.listen(3000);
